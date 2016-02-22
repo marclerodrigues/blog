f692d2cee30e7a4abe0cf16019beb8836addeece
Marcle Rodrigues <maarclee@gmail.com>
2016-02-22 20:22:11 -0300

    Login and Logout working

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
+
   root 'welcome#index'
 
   resources :users do
@@ -7,7 +9,7 @@ Rails.application.routes.draw do
     end
   end
 
-  devise_for :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
